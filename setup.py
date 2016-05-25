e88635fda953736f8991565e6bfddd97b7826038
LinkedEarth <linkedearth@gmail.com>
2016-05-25 15:44:33 -0700

    Remove long_description from setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,12 @@ import os
 import sys
 import io
 
-os.system('pandoc README.md -f markdown -t rst -s -o README.txt')
-readme_file = io.open('README.txt', encoding='utf-8')
-long_description = readme_file.open()
-os.remove('README.txt') #remove the text file
-
 setup(
     name = 'Pyleoclim',
     packages = ['Pyleoclim', 'pkg_resources'],
     version = 0.1,
     license = 'GNU Public',
     description = 'A Python package for paleoclimate data analysis',
-    long_description = long_description,
     author = 'Deborah Khider',
     author_email = 'dkhider@gmail.com',
     url = 'https://github.com/LinkedEarth/Pyleoclim_util/Pyleoclim',
