6e33ade3b91ff6a2fdef00f5828b5a9c43aaadd5
LinkedEarth <linkedearth@gmail.com>
2016-05-26 07:33:16 -0700

    Update setup.py / remove cap P
    setup.py update includes removal of long description and add required
    packages

--- Pyleoclim/start.py
+++ Pyleoclim/start.py
@@ -7,8 +7,8 @@ Created on Wed May 11 10:51:35 2016
 """
 # Import the needed modules
 from lipd.start import *
-import Pyleoclim.pkg_resources.MapLipd as MapLipd
-import Pyleoclim.pkg_resources.PlotLipd as PlotLipd
+import pyleoclim.pkg_resources.MapLipd as MapLipd
+import pyleoclim.pkg_resources.PlotLipd as PlotLipd
 
 
 # load the LiPD files
@@ -32,7 +32,7 @@ def MapaLipd(name="",gridlines = False, borders = True, \
     map1 = MapLipd.MakeMap()
     map1.map_aLiPD(name,gridlines, borders, topo, markersize, marker, \
         saveFig)
-    
+
 # PLOT TIME SERIES
 
 def PlotaLipd(filename = "", \
@@ -41,7 +41,6 @@ def PlotaLipd(filename = "", \
     """
     Plot one particular LiPD record
     """
-    
+
     plot1 = PlotLipd.MakePlot()
     plot1.plot_aLiPD(filename, plot_style, x_min, x_max, saveFig)
-            
--- setup.py
+++ setup.py
@@ -6,14 +6,20 @@ import io
 setup(
     name = 'Pyleoclim',
     packages = find_packages(),
-    version = '0.1.2',
+    version = '0.1.3',
     license = 'GNU Public',
     description = 'A Python package for paleoclimate data analysis',
-    long_description = open('README.md').read(),
     author = 'Deborah Khider',
     author_email = 'dkhider@gmail.com',
     url = 'https://github.com/LinkedEarth/Pyleoclim_util/Pyleoclim',
     download_url = 'https://github.com/LinkedEarth/Pyleoclim_util/Pyleoclim/tarball/0.1',
     keywords = ['Paleoclimate, Data Analysis'],
     classifiers = [],
+    install_requires = [
+    "LiPD>=0.1.2.7",
+    "pandas>=0.18.1",
+    "numpy>=1.11.0",
+    "matplotlib>=1.5.1",
+    "Cartopy>=0.13.1"
+    ]
 )
