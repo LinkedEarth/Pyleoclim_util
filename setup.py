0015e6ca7469da4066f1223b8fbefde39ec94a5d
LinkedEarth <linkedearth@gmail.com>
2016-05-31 14:55:02 -0700

    update to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ import sys
 import io
 import path
 
-version = '0.1.5'
+version = '0.1.6'
 
 # Read the readme file contents into variable
-os.system('pandoc README.md -f markdown -t rst -s -o README.txt')
+if sys.argv[-1] == 'publish' or sys.argv[-1] == 'publishtest':
+    os.system('pandoc README.md -f markdown -t rst -s -o README.txt')
+
 readme_file = io.open('README.txt', encoding='utf-8')
 
 # Fallback long_description in case errors with readme file.
@@ -15,8 +17,22 @@ long_description = "Welcome to Pyleoclim. Please reference the README file in th
 with readme_file:
     long_description = readme_file.read()
 
-# Remove the text file once the description is added
-os.remove('README.txt')
+# Publish the package to the live server
+if sys.argv[-1] == 'publish':
+    # Register the tarball, upload it, and trash the temp readme rst file
+    os.system('python setup.py register -r pypi')
+    os.system('python setup.py sdist upload -r pypi')
+    os.remove('README.txt')
+    sys.exit()
+
+# Publish the package to the test server
+elif sys.argv[-1] == 'publishtest':
+    # Create dist tarball, register it to test site, upload tarball, and remove temp readme file
+    os.system('python setup.py register upload -r pypitest')
+    os.system('python setup.py sdist upload -r pypitest')
+    # Trash the temp rst readme file
+    os.remove('README.txt')
+    sys.exit()
 
 setup(
     name = 'Pyleoclim',
