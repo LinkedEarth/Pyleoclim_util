b0dae16fffe6386c5a37929d0a9a14f480e01d47
khider <11758571+khider@users.noreply.github.com>
2022-05-25 18:27:21 -0700

    Update Lipd.py
    Added docstrings

--- pyleoclim/core/Lipd.py
+++ pyleoclim/core/Lipd.py
@@ -16,26 +16,36 @@ import lipd as lpd
 class Lipd:
     '''Create a Lipd object from Lipd Files
 
+
     Parameters
     ----------
 
     usr_path : str
-        path to the Lipd file(s). Can be URL (LiPD utilities only support loading one file at a time from a URL)
+    
+        Path to the Lipd file(s). Can be URL (LiPD utilities only support loading one file at a time from a URL).
         If it's a URL, it must start with "http", "https", or "ftp".
 
     lidp_dict : dict
+    
         LiPD files already loaded into Python through the LiPD utilities
 
     validate : bool
-        Validate the LiPD files upon loading. Note that for a large library this can take up to half an hour.
+    
+        Validate the LiPD files upon loading. Note that for a large library (>300files) this can take up to half an hour.
 
     remove : bool
-        If validate is True and remove is True, ignores non-valid Lipd files. Note that loading unvalidated Lipd files may result in errors for some functionalities but not all.
+    
+        If validate is True and remove is True, ignores non-valid LiPD files. Note that loading unvalidated Lipd files may result in errors for some functionalities but not all.
 
     TODO
     ----
 
     Support querying the LinkedEarth platform
+    
+    References
+    ----------
+    
+    McKay, N. P., & Emile-Geay, J. (2016). Technical Note: The Linked Paleo Data framework – a common tongue for paleoclimatology. Climate of the Past, 12, 1093-1100. 
 
     Examples
     --------
@@ -143,23 +153,33 @@ class Lipd:
         '''
         return deepcopy(self)
 
-    def to_tso(self):
-        '''Extracts all the timeseries objects to a list of LiPD tso
+    def to_tso(self, mode='paleo'):
+        '''Extracts all the variables to a list of LiPD timeseries objects
+        
+        In LiPD, timeseries objects are flatten dictionaries that contain the values for the time and variable axes as well as relevant metadata. 
+
+        Parameters
+        ----------
+        
+        mode : {'paleo','chron'}
+        
+            Whether to extract the timeseries information from the paleo tables or chron tables
 
         Returns
         -------
+        
         ts_list : list
-            List of Lipd timeseries objects as defined by LiPD utilities
+        
+            List of LiPD timeseries objects
 
-        See also
-        --------
-
-        pyleoclim.ui.LipdSeries : LiPD Series object.
-
-
+        
+        References
+        ----------
+    
+        McKay, N. P., & Emile-Geay, J. (2016). Technical Note: The Linked Paleo Data framework – a common tongue for paleoclimatology. Climate of the Past, 12, 1093-1100. 
         '''
         cwd = os.getcwd()
-        ts_list=lpd.extractTs(self.__dict__['lipd'])
+        ts_list=lpd.extractTs(self.__dict__['lipd'], mode=mode)
         os.chdir(cwd)
         return ts_list
 
@@ -167,12 +187,16 @@ class Lipd:
         '''
         Parameters
         ----------
+        
         dataSetName : str
+        
             Extract a particular dataset
 
         Returns
         -------
+        
         new : pyleoclim.Lipd
+        
             A new object corresponding to a particular dataset
 
         '''
@@ -187,21 +211,31 @@ class Lipd:
 
     def to_LipdSeriesList(self, mode='paleo'):
         '''Extracts all LiPD timeseries objects to a list of LipdSeries objects
+        
+        In LiPD, timeseries objects are flatten dictionaries that contain the values for the time and variable axes as well as relevant metadata. 
 
         Parameters
         ----------
 
         mode : {'paleo','chron'}
+        
             Whether to extract the timeseries information from the paleo tables or chron tables
 
         Returns
         -------
         res : list
+        
             A list of LiPDSeries objects
+        
+        References
+        ----------
+    
+        McKay, N. P., & Emile-Geay, J. (2016). Technical Note: The Linked Paleo Data framework – a common tongue for paleoclimatology. Climate of the Past, 12, 1093-1100. 
 
         See also
         --------
-        pyleoclim.ui.LipdSeries : LipdSeries object
+        
+        pyleoclim.core.LipdSeries.LipdSeries : a LipdSeries object
 
         '''
         cwd = os.getcwd()
@@ -232,25 +266,31 @@ class Lipd:
     def to_LipdSeries(self, number = None, mode = 'paleo'):
         '''Extracts one timeseries from the Lipd object
 
-        Note that this function may require user interaction.
+        In LiPD, timeseries objects are flatten dictionaries that contain the values for the time and variable axes as well as relevant metadata. 
+        Note that this function may require user interaction if the number of the column in the file is unknown. The numbers are fixed so automating the code is as simple as retaining a series of numbers when reopening the files. 
 
         Parameters
         ----------
 
         number : int
+        
             the number of the timeseries object
 
-        mode : {'paleo','chron'}
+        mode : str; {'paleo','chron'}
+        
             whether to extract the paleo or chron series.
 
         Returns
         -------
+        
         ts : pyleoclim.LipdSeries
+        
             A LipdSeries object
 
         See also
         --------
-        pyleoclim.ui.LipdSeries : LipdSeries object
+        
+        pyleoclim.core.LipdSeries.LipdSeries : LipdSeries object
 
         '''
         cwd = os.getcwd()
@@ -274,53 +314,88 @@ class Lipd:
            markersize = None, scatter_kwargs=None,
            legend=True, lgd_kwargs=None, savefig_settings=None):
 
-        '''Map the records contained in LiPD files by archive type
+        '''Map all the records contained in the LiPD object by the type of archive
+        
+        Note that the map is fully cusomizable by using the optional parameters. 
 
         Parameters
         ----------
         projection : str, optional
+        
             The projection to use. The default is 'Robinson'.
+            
         proj_default : bool, optional
+        
             Wether to use the Pyleoclim defaults for each projection type. The default is True.
+            
         background : bool, optional
+        
             Wether to use a backgound. The default is True.
+            
         borders : bool, optional
+        
             Draw borders. The default is False.
+            
         rivers : bool, optional
+        
             Draw rivers. The default is False.
+            
         lakes : bool, optional
+        
             Draw lakes. The default is False.
+            
         figsize : list, optional
+        
             The size of the figure. The default is None.
+            
         ax : matplotlib.ax, optional
+        
             The matplotlib axis onto which to return the map. The default is None.
+            
         marker : str, optional
-            The marker type for each archive. The default is None. Uses plot_default
+        
+            The marker type for each archive. The default is None, which uses a pre-defined palette in Pyleoclim.  
+            To see the default option, run Lipd.plot_default where Lipd is the name of the object. 
+            
         color : str, optional
-            Color for each acrhive. The default is None. Uses plot_default
+        
+            Color for each acrhive. The default is None. The default is None, which uses a pre-defined palette in Pyleoclim.  
+            To see the default option, run Lipd.plot_default where Lipd is the name of the object. 
+            
         markersize : float, optional
-            Size of the marker. The default is None.
+        
+            Size of the marker. The default is None. 
+            
         scatter_kwargs : dict, optional
+        
             Parameters for the scatter plot. The default is None.
-        legend : bool, optional
+            
+        legend : bool; {True,False}, optional
+        
             Whether to plot the legend. The default is True.
+            
         lgd_kwargs : dict, optional
+        
             Arguments for the legend. The default is None.
+            
         savefig_settings : dictionary, optional
-            the dictionary of arguments for plt.savefig(); some notes below:
-            - "path" must be specified; it can be any existed or non-existed path,
+        
+            The dictionary of arguments for plt.savefig(); some notes below:  
+            - "path" must be specified; it can be any existing or non-existing path,
               with or without a suffix; if the suffix is not given in "path", it will follow "format"
-            - "format" can be one of {"pdf", "eps", "png", "ps"}. The default is None.
+            - "format" can be one of {"pdf", "eps", "png", "ps"}. 
+            The default is None.
         
         Returns
         -------
-        res : figure
-            The figure
+        res : tuple or fig
+        
+            The figure and axis if asked. 
 
         See also
         --------
 
-        pyleoclim.utils.mapping.map_all : Underlying mapping function for Pyleoclim
+        pyleoclim.utils.mapping.map : Underlying mapping function for Pyleoclim
 
         Examples
         --------
