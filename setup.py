6b0afd24f57f40154f18ab04961145e7928473c4
CommonClimate <julieneg@usc.edu>
2022-08-16 15:13:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires=[
         "LiPD==0.2.8.8",
         "pandas>=1.3.0",
-        "numpy>=1.21.1",
+        "numpy==1.22.0",
         "matplotlib>=3.4.2",
         "scipy>=1.7.0",
         "statsmodels>=0.11.1",
