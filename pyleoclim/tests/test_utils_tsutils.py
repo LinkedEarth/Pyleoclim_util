2068a4258461361488617bd69ee073e1a57ba779
Alexander James <akeenjames@gmail.com>
2023-01-30 11:39:08 -0800

    add tsutils.bin tests
    Added tests for tsutils.bin to:
    * Test if it produced objects of nonzero length
    * Test if it produced np.ndarray objects
    * Test if it produced evenly spaced time axis when requested

--- pyleoclim/tests/test_utils_tsutils.py
+++ pyleoclim/tests/test_utils_tsutils.py
@@ -1,6 +1,6 @@
 
 import pytest
-from pyleoclim.utils import tsutils
+from pyleoclim.utils import tsutils, tsbase
 import numpy as np
 
 
@@ -143,3 +143,22 @@ def test_convert_datetime_index_nondt_index(dataframe):
             time_unit, 
             time_name=time_name,
             )
+
+def test_bin_t0(unevenly_spaced_series):
+    res_dict = tsutils.bin(unevenly_spaced_series.time,unevenly_spaced_series.value)
+    t = res_dict['bins']
+    v = res_dict['binned_values']
+    assert len(t) > 0
+    assert len(v) > 0
+
+def test_bin_t1(unevenly_spaced_series):
+    res_dict = tsutils.bin(unevenly_spaced_series.time,unevenly_spaced_series.value)
+    t = res_dict['bins']
+    v = res_dict['binned_values']
+    assert isinstance(t,np.ndarray)
+    assert isinstance(v,np.ndarray)
+
+def test_bin_t2(unevenly_spaced_series):
+    res_dict = tsutils.bin(unevenly_spaced_series.time,unevenly_spaced_series.value,evenly_spaced=True)
+    t = res_dict['bins']
+    assert tsbase.is_evenly_spaced(t)
