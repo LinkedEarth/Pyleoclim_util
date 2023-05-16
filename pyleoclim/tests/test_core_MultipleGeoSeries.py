047e65b46c58c809a09606ced4e67a6ac230402c
jlanders <jordan.landers+dev@gmail.com>
2023-05-16 14:59:01 -0700

    commented out bs4 import

--- pyleoclim/tests/test_core_MultipleGeoSeries.py
+++ pyleoclim/tests/test_core_MultipleGeoSeries.py
@@ -6,7 +6,7 @@ Created on Thu May 11 09:47:08 2023
 @author: julieneg
 """
 import pyleoclim as pyleo
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import requests
 import pytest
 
