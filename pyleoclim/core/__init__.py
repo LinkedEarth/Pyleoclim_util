f4197dad5f986c4976b0e81d26d93911b46904ec
Alexander James <akeenjames@gmail.com>
2024-03-05 12:21:01 -0800

    add resolution and multipleresolution to init

--- pyleoclim/core/__init__.py
+++ pyleoclim/core/__init__.py
@@ -19,3 +19,5 @@ from .multivardecomp import MultivariateDecomp
 from .ssares import SsaRes
 from .lipd import Lipd
 from .lipdseries import LipdSeries
+from .resolution import Resolution
+from .multipleresolution import MultipleResolution
