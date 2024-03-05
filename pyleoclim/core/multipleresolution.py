ee76ee25f1953e1c0e890df1afa36ba04e0befe1
Alexander James <akeenjames@gmail.com>
2024-03-05 12:20:48 -0800

    create multipleresolution class

---
+++ pyleoclim/core/multipleresolution.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+MultipleResolution objects are designed to contain, display, and analyze information on the resolution of multiple time axes from a MultipleSeries, EnsembleSeries, or SurrogateSeries object.
+"""
+
+from ..utils import tsutils, plotting, tsmodel, tsbase
+
+import warnings
+
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import scipy.stats as st
+import matplotlib.pyplot as plt
+
+from matplotlib import gridspec
+
+class MultipleResolution:
+    ''' Resolution object
+    
+    Resolution objects store time axis resolution information derived from MultipleSeries objects.
+    They are generated via the resolution method applied to a MultipleSeries object and contain methods relevant to the analysis of resolution information.
+
+    See Also
+    --------
+
+    '''
+
+    def __init__(self,resolution_list):
+        self.resolution_list = resolution_list
