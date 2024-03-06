cdf6322a46a7c00c1dd4f83dd7f17c67f8905c34
Alexander James <akeenjames@gmail.com>
2024-03-06 14:28:09 -0800

    msr tests

---
+++ pyleoclim/tests/test_core_MultipleResolution.py
@@ -0,0 +1,74 @@
+''' Tests for pyleoclim.core.ui.MultipleResolution
+
+Naming rules:
+1. class: Test{filename}{Class}{method} with appropriate camel case
+2. function: test_{method}_t{test_id}
+
+Notes on how to test:
+0. Make sure [pytest](https://docs.pytest.org) has been installed: `pip install pytest`
+1. execute `pytest {directory_path}` in terminal to perform all tests in all testing files inside the specified directory
+2. execute `pytest {file_path}` in terminal to perform all tests in the specified file
+3. execute `pytest {file_path}::{TestClass}::{test_method}` in terminal to perform a specific test class/method inside the specified file
+4. after `pip install pytest-xdist`, one may execute "pytest -n 4" to test in parallel with number of workers specified by `-n`
+5. for more details, see https://docs.pytest.org/en/stable/usage.html
+'''
+
+import pytest
+import pyleoclim as pyleo
+
+class TestUIMultipleResolutionSummaryPlot:
+    def test_plot_t0(self,multipleseries_basic):
+        '''
+        test resolution plot
+        '''
+        ms = multipleseries_basic
+        msr = ms.resolution()
+        fig,ax=msr.summary_plot()
+        pyleo.closefig(fig)
+    def test_plot_t1(self,multipleseries_nans):
+        '''
+        test resolution plot with nans
+        '''
+        ms = multipleseries_nans # create MS object
+        ms.resolution()
+        msr = ms.resolutoin()
+        fig,ax=msr.summary_plot()
+        pyleo.closefig(fig)
+    def test_plot_t2(self):
+        '''
+        test resolution plot with time unit
+        '''
+        co2ts = pyleo.utils.load_dataset('AACO2')
+        lr04 = pyleo.utils.load_dataset('LR04')
+        edc = pyleo.utils.load_dataset('EDC-dD')
+        ms = lr04.flip() & edc & co2ts # create MS object
+        msr = ms.resolution(time_unit='kyr BP')
+        fig,ax=msr.summary_plot()
+        pyleo.closefig(fig)
+
+class TestUIMultipleResolutionDescribe:
+    def test_describe_t0(self,multipleseries_basic):
+        '''
+        test resolution describe
+        '''
+        ms = multipleseries_basic
+        msr = ms.resolution()
+        msr.describe()
+    def test_describe_t1(self,multipleseries_nans):
+        '''
+        test resolution describe with nans
+        '''
+        ms = multipleseries_nans # create MS object
+        ms.resolution()
+        msr = ms.resolutoin()
+        msr.describe()
+    def test_describe_t2(self):
+        '''
+        test resolution plot with time unit
+        '''
+        co2ts = pyleo.utils.load_dataset('AACO2')
+        lr04 = pyleo.utils.load_dataset('LR04')
+        edc = pyleo.utils.load_dataset('EDC-dD')
+        ms = lr04.flip() & edc & co2ts # create MS object
+        msr = ms.resolution(time_unit='kyr BP')
+        msr.describe()
