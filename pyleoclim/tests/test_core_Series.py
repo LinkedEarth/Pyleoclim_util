9b1668f0e4f42cdd7be14d9bf2b848cd899c962c
CommonClimate <julieneg@usc.edu>
2025-01-07 14:53:58 -0800

    Test r_crit against analytical solution

--- pyleoclim/tests/test_core_Series.py
+++ pyleoclim/tests/test_core_Series.py
@@ -28,6 +28,7 @@ test_dirpath = pathlib.Path(__file__).parent.absolute()
 
 import pyleoclim as pyleo
 import pyleoclim.utils.tsbase as tsbase
+import pyleoclim.utils.tsmodel as tsmodel
 
 
 
@@ -697,6 +698,31 @@ class TestUISeriesCorrelation:
         ts_13 = pyleo.utils.load_dataset('cenogrid_d13C')
         corr = gmst.correlation(ts_13)
         assert np.isnan(corr.r)
+        
+    def test_correlation_t5(self):
+        '''
+        Check that `ttest` method returns an analytically-sensible critical value
+        '''
+        ts=pyleo.utils.load_dataset('SOI').standardize()
+        rho = 0.6; tcrit = 1.96  # use quantile of standard normal bc nu is so large
+        # generate series whose correlation with ts1 should be close to rho:
+        v2 = rho*ts.value + np.sqrt(1-rho**2)*np.random.normal(loc=0, scale=1, size=len(ts.time))
+        ts2 = pyleo.Series(time=ts.time, value=v2, verbose=False, auto_time_params=True)
+        corr_res = ts.correlation(ts2, method= 'ttest')
+        import scipy.stats as stats
+        g1 = tsmodel.ar1_fit_evenly(ts.value)
+        g2 = tsmodel.ar1_fit_evenly(v2)
+        N = len(ts.value)
+        
+        Ney1 = N * (1-g1) / (1+g1)
+        Ney2 = N * (1-g2) / (1+g2)
+        
+        Ne = stats.mstats.gmean([Ney1,Ney2])
+        df = Ne -2
+        
+        r_crit = np.sqrt(tcrit**2/(df*(1+tcrit**2)))
+                  
+        assert np.abs((corr_res.r_crit -r_crit)/r_crit) < 0.1
         
 
 class TestUISeriesCausality:
